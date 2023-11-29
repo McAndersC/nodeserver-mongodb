7b7c08c40df9428fbc17843e0e48ee41cf7414cb
Anders Christensen <anders@medieskolerne.dk>
2023-11-29 17:57:59 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+import server from "./lib/server.js"
+
+const app = {};
+
+app.init = () => {
+
+     const port = 3000;
+
+     server.listen(3000, () => {
+
+          console.log('\n------ SERVER ------ \n')
+          console.log('http://localhost:' + port)
+          console.log('\n------ ****** ------ \n')
+
+     })
+
+
+}
+
+// Starter Applikation
+app.init();
